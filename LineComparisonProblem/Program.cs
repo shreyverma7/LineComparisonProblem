c0171563630af34040713579a154365c9b43ad90
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-12 11:35:30 +0530

    [Shrey] Add : Folder creation

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LineComparisonProblem
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            //UC1
+            Console.WriteLine("Welcome to Line Comparsion Problem statement");
+            Operation operation = new Operation(1,2,3,4);
+            operation.CalculateLength();
+           
+        }
+    }
+}
